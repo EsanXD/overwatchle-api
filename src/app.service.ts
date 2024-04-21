a5dfe0e8b1a7e393b69f6fefe8fe4e063912093f
EsanXD <tran.ethann@gmail.com>
2024-04-21 09:55:21 -0500

    change to characters instead of abilities to balance

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { abilities } from './abilities';
+import { characters } from './characters';
+
 const seedrandom = require('seedrandom');
-const { DateTime } = require('luxon');
 
 @Injectable()
 export class AppService {
   getDaily(date: string): string {
     // seed will be date in format 'dd/mm/yyyy'
     console.log(date);
-    const indexes: number[] = generateRandomIndexes(date, abilities.length);
-    const dailyAbilities = indexes.map((index) => abilities[index]);
+    const indexes: number[] = generateRandomIndexes(date, characters.length);
+    const dailyAbilities = indexes.map((index) => characters[index]);
     return btoa(btoa(btoa(JSON.stringify(dailyAbilities).toLowerCase())));
     // return JSON.stringify(dailyAbilities).toLowerCase();
   }
@@ -18,7 +18,7 @@ export class AppService {
 const generateRandomIndexes = (seed, length): number[] => {
   const rng = seedrandom(seed);
   const indexes = new Set<number>();
-  while (indexes.size < 3) {
+  while (indexes.size < 1) {
     const index = Math.floor((rng() * 10000000) % length);
     indexes.add(index);
   }
